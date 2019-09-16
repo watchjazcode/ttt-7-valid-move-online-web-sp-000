4ae5065a70e4e45004e65a4ab8fa31b5c7364549
Jazmin M <jazminmerckel@gmail.com>
2019-09-16 03:04:49 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,22 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    return true
+  else 
+    return false
+  end
+end
 
+#! needed to make position_taken? opposite value to make entire condition true
+#.between? method helps us to have a range between 0 - 8 (already converted from
+# user input) for our board.
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == "" || board[index] == " " || board[index] == nil
+    return false
+  else 
+    return true
+  end
+end
+  
